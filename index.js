78f6aadbbb36d542d6647e35b2440d444e425318
gamegine <gamegine@protonmail.com>
2022-02-04 00:00:55 +0000

    discord events error ready

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// imports
 const fs = require("fs")
 const Discord = require("discord.js")
 
@@ -7,6 +8,18 @@ const config = fs.existsSync("./config.json") ? require("./config.json") : {}
 // load discord token from config or env
 const token = config.DISCORD_TOKEN || process.env.DISCORD_TOKEN
 
+// create new discord client with token
 const client = new Discord.Client()
 
+// intercept discord events
+
+// on error display formated error
+// eslint-disable-next-line no-console
+client.on("error", (err) => console.error(`Error: ${err.message}`))
+
+// log when discord client is ready
+// eslint-disable-next-line no-console
+client.on("ready", () => console.log(`Logged in as ${client.user.tag}!`))
+
+// start discord client by login with token
 client.login(token)
