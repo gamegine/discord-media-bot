ff5f459404574fcd4b3575f3c7f6c3bd44fdb03a
gamegine <gamegine@protonmail.com>
2022-02-04 16:34:24 +0000

    discord on message call module system & reply

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // imports
 const fs = require("fs")
 const Discord = require("discord.js")
-const { getUrls } = require("./functions")
+const { getUrls, moduleImportAll } = require("./functions")
+// import all modules and log imported modules
+const modules = moduleImportAll(`${__dirname}/modules`)
+// eslint-disable-next-line no-console
+modules.forEach((module) => console.info(`imported module: ${module.file}`))
 
 // load config.json if exists
 // eslint-disable-next-line import/no-unresolved
@@ -32,8 +36,22 @@ client.on("message", (msg) => {
 	const urls = getUrls(text)
 	// if no urls found ignore message
 	if (!urls.length) return
-	// eslint-disable-next-line no-console
-	console.log(urls)
+	// if no modules processinng ignore message
+	if (!modules.length) return
+	// init all modules onUrls function and add it to promise chain
+	const promises = modules.map((module) => module.module.onUrls(urls))
+	// run all onUrls promises
+	Promise.all(promises)
+		.then((results) => {
+			// merge all results into one
+			const json = results.reduce((acc, result) => ({ ...acc, ...result }))
+			// reply to discord with json data
+			msg
+				.reply(json.urls, { files: json.files })
+				.then(() => {})
+				.catch((e) => console.error("error discord reply :", e))
+		})
+		.catch((e) => console.error("error modules onUrls : ", e))
 })
 
 // start discord client by login with token
